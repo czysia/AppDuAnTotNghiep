879fdc0798fe77d33d3e815fe3c4785748dbd43f
czysia <nson02902@gmail.com>
2022-02-12 21:02:04 +0700

    SonNguyen

---
+++ app/src/main/java/org/sonnnph12414/appduantotnghiep/MainActivity.java
@@ -0,0 +1,14 @@
+package org.sonnnph12414.appduantotnghiep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
