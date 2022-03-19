9003dbb99e74ee73344d4eec955eb6ecdbd6363d
ASUS <kienltph11232@fpt.edu.vn>
2022-03-19 19:13:58 +0700

    kienltph11232

--- app/src/main/java/com/example/duantotnghiep/ManHinhChaoActivity.java
+++ app/src/main/java/com/example/duantotnghiep/ManHinhChaoActivity.java
@@ -14,20 +14,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class ManHinhChaoActivity extends AppCompatActivity {
 
-    private static int SPLASH_SCREEN_TIMEOUT = 2000;
+    private static int SPLASH_SCREEN_TIMEOUT = 3000;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
+
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.manhinhchao_activity);
 
+
         Animation fadeOut = new AlphaAnimation(1, 0);
         fadeOut.setInterpolator(new AccelerateInterpolator());
-        fadeOut.setStartOffset(500);
-        fadeOut.setDuration(1800);
+        fadeOut.setStartOffset(300);
+        fadeOut.setDuration(2700);
         ImageView image = findViewById(R.id.imageView);
 
         image.setAnimation(fadeOut);
@@ -40,6 +44,7 @@ public class ManHinhChaoActivity extends AppCompatActivity {
                 finish();
             }
         }, SPLASH_SCREEN_TIMEOUT);
+
     }
 
 
