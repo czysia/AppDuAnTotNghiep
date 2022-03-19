9dc19996ddefb40a159636d815f412db2c446670
ASUS <kienltph11232@fpt.edu.vn>
2022-03-19 16:25:56 +0700

    kienltph11232

---
+++ app/src/main/java/com/example/duantotnghiep/ManHinhChaoActivity.java
@@ -0,0 +1,46 @@
+package com.example.duantotnghiep;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ManHinhChaoActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN_TIMEOUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.manhinhchao_activity);
+
+        Animation fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setInterpolator(new AccelerateInterpolator());
+        fadeOut.setStartOffset(500);
+        fadeOut.setDuration(1800);
+        ImageView image = findViewById(R.id.imageView);
+
+        image.setAnimation(fadeOut);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(ManHinhChaoActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIMEOUT);
+    }
+
+
+}
