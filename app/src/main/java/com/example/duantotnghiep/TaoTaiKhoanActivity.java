36556025d1d042f331e107e55fe56bdefe3813ee
ASUS <kienltph11232@fpt.edu.vn>
2022-03-21 21:49:28 +0700

    kienltph11232

---
+++ app/src/main/java/com/example/duantotnghiep/TaoTaiKhoanActivity.java
@@ -0,0 +1,14 @@
+package com.example.duantotnghiep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TaoTaiKhoanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tao_tai_khoan);
+    }
+}
