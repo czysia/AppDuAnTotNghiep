6aaa40f7b65c11b38c91b75ed8558398c6d281b1
ASUS <kienltph11232@fpt.edu.vn>
2022-03-25 14:19:51 +0700

    kienltph11232

---
+++ app/src/main/java/com/example/duantotnghiep/QuenMatKhauActivity.java
@@ -0,0 +1,14 @@
+package com.example.duantotnghiep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuenMatKhauActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quen_mat_khau);
+    }
+}
--- app/src/main/java/com/example/duantotnghiep/TrangChuActivity.java
+++ app/src/main/java/com/example/duantotnghiep/TrangChuActivity.java
@@ -4,11 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.duantotnghiep.Adapter.SliderAdapter;
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+
 public class TrangChuActivity extends AppCompatActivity {
+    SliderView sliderView;
+    int[] images = {R.drawable.slide1,
+    R.drawable.slide2,
+    R.drawable.slide3,
+    R.drawable.slide4,
+    R.drawable.slide5};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trang_chu);
+
+        sliderView = findViewById(R.id.imageSlider);
+
+        SliderAdapter sliderAdapter = new SliderAdapter(images);
+
+        sliderView.setSliderAdapter(sliderAdapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+        sliderView.startAutoCycle();
     }
 }
