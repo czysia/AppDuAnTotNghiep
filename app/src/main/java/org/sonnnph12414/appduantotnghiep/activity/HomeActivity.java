538528df015c55129249dc959dc29c6df19fc53c
trungtang <trungtvph10471@fpt.edu.vn>
2022-03-26 21:27:16 +0700

    update Home

---
+++ app/src/main/java/org/sonnnph12414/appduantotnghiep/activity/HomeActivity.java
@@ -0,0 +1,14 @@
+package org.sonnnph12414.appduantotnghiep.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
