df419a8af78227c481c1387f37cff6d33a696eba
QUANTVPH09679 <57596313+QUANTVPH09679@users.noreply.github.com>
2022-03-27 17:11:55 +0700

    update splashScreen

--- app/src/main/java/org/sonnnph12414/appduantotnghiep/activity/SplashScreen.java
+++ app/src/main/java/org/sonnnph12414/appduantotnghiep/activity/SplashScreen.java
@@ -8,16 +8,17 @@ import android.os.Handler;
 
 import org.sonnnph12414.appduantotnghiep.R;
 
-public class SplashScreenActivity extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity {
     public static int SPLASH_TIME = 3000; //This is 3 seconds
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent splashScreen = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                Intent splashScreen = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(splashScreen);
                 finish();
             }
