839d7866543d0dbccc25dcee0c5c41e0ba1dfd63
QUANTVPH09679 <57596313+QUANTVPH09679@users.noreply.github.com>
2022-03-28 12:47:22 +0700

    update splashScreen

---
+++ app/src/main/java/org/sonnnph12414/appduantotnghiep/model/RequestUpdate.java
@@ -0,0 +1,9 @@
+package org.sonnnph12414.appduantotnghiep.model;
+
+public class RequestUpdate {
+    private User updateUser;
+
+    public void setUser(User user){
+        this.updateUser = user;
+    }
+}
