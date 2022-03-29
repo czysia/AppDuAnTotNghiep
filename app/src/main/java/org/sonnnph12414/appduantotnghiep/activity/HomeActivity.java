b9f15c247cfc6345b0a46e6619fadd4392e8c029
sonlvph10214 <levansonnkc@gmail.com>
2022-03-29 07:02:05 +0700

    commit fix màn hinh home và update menu

--- app/src/main/java/org/sonnnph12414/appduantotnghiep/activity/HomeActivity.java
+++ app/src/main/java/org/sonnnph12414/appduantotnghiep/activity/HomeActivity.java
@@ -1,18 +1,24 @@
 package org.sonnnph12414.appduantotnghiep.activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
 
@@ -22,6 +28,8 @@ import com.google.android.material.navigation.NavigationView;
 import org.sonnnph12414.appduantotnghiep.R;
 import org.sonnnph12414.appduantotnghiep.adapter.CategoriesAdapter;
 import org.sonnnph12414.appduantotnghiep.api.APIClient;
+import org.sonnnph12414.appduantotnghiep.fragment.FoodFragment;
+import org.sonnnph12414.appduantotnghiep.fragment.HomeFragment;
 import org.sonnnph12414.appduantotnghiep.model.Categories;
 
 import java.util.ArrayList;
@@ -34,84 +42,80 @@ import retrofit2.Response;
 import org.sonnnph12414.appduantotnghiep.R;
 
 public class HomeActivity extends AppCompatActivity {
-    Toolbar toolbar;
-    ViewFlipper viewFlipper;
-    RecyclerView recyclerViewManHinhChinh;
-    NavigationView navigationView;
-    ListView listViewManHinhChinh;
-    DrawerLayout drawerLayout;
-    List<Categories> categoriesList;
+    public DrawerLayout drawerLayout;
+
+    private int currentFragment=FRAGMENT_HOME;
+    private  static final int FRAGMENT_HOME =0;
+    private  static final int FRAGMENT_FOOD =1;
+    private  static final int FRAGMENT_INFO =2;
+    private  static final int FRAGMENT_SIGNOUT =3;
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Anhxa();
-        ActionBar();
-        ActionViewFlipper();
-        categoriesList= new ArrayList<>();
-        viewCategories();
-    }
-    private void ActionViewFlipper() {
-        List<String> mangquangcao = new ArrayList<>();
-        mangquangcao.add("http://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-Le-hoi-phu-kien-800-300.png");
-        mangquangcao.add("http://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-HC-Tra-Gop-800-300.png");
-        mangquangcao.add("http://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-big-ky-nguyen-800-300.jpg");
-        for (int i = 0; i<mangquangcao.size(); i++ ){
-            ImageView imageView = new ImageView(getApplicationContext());
-            Glide.with(getApplicationContext()).load(mangquangcao.get(i)).into(imageView);
-            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-            viewFlipper.addView(imageView);
-        }
-        viewFlipper.setFlipInterval(3000);
-        viewFlipper.setAutoStart(true);
-        Animation slide_in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_in_right);
-        Animation slide_out = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_cut_right);
-        viewFlipper.setInAnimation(slide_in);
-        viewFlipper.setOutAnimation(slide_out);
-    }
 
-    private void ActionBar() {
+        drawerLayout = findViewById(R.id.drawerLayout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        toolbar.setNavigationIcon(android.R.drawable.ic_menu_sort_by_size);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,
+                toolbar,R.string.nav_open,R.string.nav_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        FragmentManager manager = getSupportFragmentManager();
+        HomeFragment homeFragment = new HomeFragment();
+        manager.beginTransaction()
+                .replace(R.id.content_fame, homeFragment)
+                .commit();
+
+        NavigationView navigationView = findViewById(R.id.navigationView);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view){
-                drawerLayout.openDrawer(GravityCompat.START);
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                FragmentManager manager = getSupportFragmentManager();
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        setTitle("Trang chủ");
+                        HomeFragment homeFragment1 = new HomeFragment();
+                        manager.beginTransaction()
+                                .replace(R.id.content_fame, homeFragment1)
+                                .commit();
+                        break;
+                    case R.id.nav_food:
+                        setTitle("Đồ ăn");
+                        FoodFragment foodFragment = new FoodFragment();
+                        manager.beginTransaction()
+                                .replace(R.id.content_fame, foodFragment)
+                                .commit();
+                        break;
+                    case R.id.nav_info:
+
+                    case R.id.nav_sign_out:
+
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return false;
             }
         });
+
+
     }
 
-    private void Anhxa() {
-        toolbar = findViewById(R.id.toobarmanhinhchinh);
-        viewFlipper = findViewById(R.id.viewlipper);
-        recyclerViewManHinhChinh = findViewById(R.id.recycleview);
-        listViewManHinhChinh = findViewById(R.id.listviewmanhinhchinh);
-        navigationView = findViewById(R.id.navigationview);
-        drawerLayout = findViewById(R.id.drawerlayout);
-        //khoi tao list
-        if (APIClient.manggiohang == null){APIClient.manggiohang = new ArrayList<>();}
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
     }
-    private void viewCategories() {
-        Call<List<Categories>> call = APIClient.create().getAllCategories();
-        call.enqueue(new Callback<List<Categories>>() {
-            @Override
-            public void onResponse(Call<List<Categories>> call, Response<List<Categories>> response) {
-                categoriesList=response.body();
-                GridLayoutManager gridLayoutManager = new GridLayoutManager(getApplicationContext(),2,RecyclerView.HORIZONTAL,false);
-                CategoriesAdapter categoriesAdapter = new CategoriesAdapter(categoriesList,getApplicationContext());
-                recyclerViewManHinhChinh.setLayoutManager(gridLayoutManager);
-                recyclerViewManHinhChinh.setAdapter(categoriesAdapter);
 
-            }
-
-            @Override
-            public void onFailure(Call<List<Categories>> call, Throwable t) {
-                Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
-
-            }
-        });
-    }
 }
---
+++ app/src/main/java/org/sonnnph12414/appduantotnghiep/fragment/HomeFragment.java
@@ -0,0 +1,112 @@
+package org.sonnnph12414.appduantotnghiep.fragment;
+
+import android.os.Bundle;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.navigation.NavigationView;
+
+import org.sonnnph12414.appduantotnghiep.R;
+import org.sonnnph12414.appduantotnghiep.adapter.CategoriesAdapter;
+import org.sonnnph12414.appduantotnghiep.api.APIClient;
+import org.sonnnph12414.appduantotnghiep.model.Categories;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+
+public class HomeFragment extends Fragment {
+    Toolbar toolbar;
+    ViewFlipper viewFlipper;
+    RecyclerView recyclerViewManHinhChinh;
+    NavigationView navigationView;
+    ListView listViewManHinhChinh;
+    DrawerLayout drawerLayout;
+    List<Categories> categoriesList;
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+         view =inflater.inflate(R.layout.fragment_home, container, false);
+
+        anhXa();
+        ActionViewFlipper();
+        categoriesList= new ArrayList<>();
+        viewCategories();
+
+        return view;
+    }
+    private void ActionViewFlipper() {
+        List<String> mangquangcao = new ArrayList<>();
+        mangquangcao.add("http://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-Le-hoi-phu-kien-800-300.png");
+        mangquangcao.add("http://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-HC-Tra-Gop-800-300.png");
+        mangquangcao.add("http://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-big-ky-nguyen-800-300.jpg");
+        for (int i = 0; i<mangquangcao.size(); i++ ){
+            ImageView imageView = new ImageView(getActivity());
+            Glide.with(getActivity()).load(mangquangcao.get(i)).into(imageView);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            viewFlipper.addView(imageView);
+        }
+        viewFlipper.setFlipInterval(3000);
+        viewFlipper.setAutoStart(true);
+        Animation slide_in = AnimationUtils.loadAnimation(getActivity(), R.anim.slide_in_right);
+        Animation slide_out = AnimationUtils.loadAnimation(getActivity(), R.anim.slide_cut_right);
+        viewFlipper.setInAnimation(slide_in);
+        viewFlipper.setOutAnimation(slide_out);
+    }
+
+
+
+    private void anhXa() {
+        toolbar = view.findViewById(R.id.toobarmanhinhchinh);
+        viewFlipper = view.findViewById(R.id.viewlipper);
+        recyclerViewManHinhChinh = view.findViewById(R.id.recycleview);
+        listViewManHinhChinh = view.findViewById(R.id.listviewmanhinhchinh);
+        navigationView = view.findViewById(R.id.navigationview);
+        drawerLayout = view.findViewById(R.id.drawerlayout);
+        //khoi tao list
+        if (APIClient.manggiohang == null){APIClient.manggiohang = new ArrayList<>();}
+    }
+    private void viewCategories() {
+        Call<List<Categories>> call = APIClient.create().getAllCategories();
+        call.enqueue(new Callback<List<Categories>>() {
+            @Override
+            public void onResponse(Call<List<Categories>> call, Response<List<Categories>> response) {
+                categoriesList=response.body();
+                GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(),2,RecyclerView.HORIZONTAL,false);
+                CategoriesAdapter categoriesAdapter = new CategoriesAdapter(categoriesList,getActivity());
+                recyclerViewManHinhChinh.setLayoutManager(gridLayoutManager);
+                recyclerViewManHinhChinh.setAdapter(categoriesAdapter);
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Categories>> call, Throwable t) {
+                Toast.makeText(getActivity(), t.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
