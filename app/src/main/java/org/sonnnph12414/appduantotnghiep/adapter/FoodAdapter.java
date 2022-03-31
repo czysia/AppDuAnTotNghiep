91b754cad972f742350f1908a19188d2d9f1a180
czysia <nson02902@gmail.com>
2022-03-31 21:48:48 +0700

    (ngocson) gio hang

--- app/src/main/java/org/sonnnph12414/appduantotnghiep/adapter/FoodAdapter.java
+++ app/src/main/java/org/sonnnph12414/appduantotnghiep/adapter/FoodAdapter.java
@@ -2,6 +2,8 @@ package org.sonnnph12414.appduantotnghiep.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,13 +11,18 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 
+import org.greenrobot.eventbus.EventBus;
 import org.sonnnph12414.appduantotnghiep.R;
 import org.sonnnph12414.appduantotnghiep.activity.ChiTietActivity;
+import org.sonnnph12414.appduantotnghiep.activity.GioHangActivity;
+import org.sonnnph12414.appduantotnghiep.fragment.ChiTietFragment;
 import org.sonnnph12414.appduantotnghiep.model.Food;
+import org.sonnnph12414.appduantotnghiep.model.Foodbuy;
 
 import java.util.List;
 
@@ -34,6 +41,7 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.ViewHolder>{
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_food,parent,false);
+
         return new ViewHolder(view);
     }
 
@@ -52,10 +60,28 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.ViewHolder>{
         holder.imgFood.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+//                 chua chuyen du lieu + mo fragment moi
                 if (!view.isLongClickable()){
                     Intent intent = new Intent(context, ChiTietActivity.class);
-                    intent.putExtra("chitiet",food);
+                    Fragment fragment = new Fragment();
+                    Bundle bundle = new Bundle();
+                    bundle.putString("chitietName", String.valueOf(holder.tvFoodName));
+                    bundle.putString("chitietPrice", String.valueOf(holder.tvFoodPrice));
+                    bundle.putString("chitietInfo", String.valueOf(holder.tvFoodInfo));
+                    bundle.putString("chitietImg", String.valueOf(holder.imgFood));
+                    fragment.setArguments(bundle);
+                    Log.e("send","da chuyen");
+
                 }
+                sendDatatoFragment();
+            }
+            private void sendDatatoFragment() {
+                Foodbuy foodbuy = new Foodbuy(String.valueOf(holder.tvFoodName),String.valueOf(holder.tvFoodPrice),
+                        String.valueOf(holder.tvFoodInfo),String.valueOf(holder.imgFood));
+                EventBus.getDefault().postSticky(foodbuy);
+
+                Intent i = new Intent(holder.imgFood.getContext(), GioHangActivity.class);
+                context.startActivity(i);
             }
         });
 
@@ -69,7 +95,12 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.ViewHolder>{
         return 0;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    @Override
+    public long getItemId(int position) {
+        return super.getItemId(position);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder  {
         TextView tvFoodName,tvFoodPrice,tvFoodInfo;
         ImageView imgFood;
         public ViewHolder(@NonNull View itemView) {
