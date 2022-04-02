5a542515516573ec0c4deddfd4a6569f976c75f7
czysia <nson02902@gmail.com>
2022-04-02 19:40:21 +0700

    (ngocson) gio hang

--- app/src/main/java/org/sonnnph12414/appduantotnghiep/adapter/GioHangAdapter.java
+++ app/src/main/java/org/sonnnph12414/appduantotnghiep/adapter/GioHangAdapter.java
@@ -1,12 +1,20 @@
 package org.sonnnph12414.appduantotnghiep.adapter;
 
 import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
+import org.sonnnph12414.appduantotnghiep.R;
+import org.sonnnph12414.appduantotnghiep.api.IimageClickListen;
 import org.sonnnph12414.appduantotnghiep.model.GioHang;
 
 import java.util.List;
@@ -14,27 +22,93 @@ import java.util.List;
 public class GioHangAdapter extends RecyclerView.Adapter<GioHangAdapter.MyViewHoler> {
     Context context;
     List<GioHang> giohangList;
+    int soluongmoitru;
+
+    public GioHangAdapter(List<GioHang> giohangList, Context context) {
+        this.giohangList = giohangList;
+        this.context = context;
+
+    }
 
     @NonNull
     @Override
     public MyViewHoler onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_giohang,parent,false);
+        return new MyViewHoler(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHoler holder, int position) {
 
+        GioHang gioHang = giohangList.get(position);
+        Glide.with(context).load(gioHang.getHinhsp()).into(holder.imgGiohangimgsp);
+        holder.tvGiohangtensp.setText(gioHang.getTensp());
+        holder.tvGiohanggiatien.setText(String.valueOf(gioHang.getGiasp()));
+        holder.tvGiohangsoluong.setText(String.valueOf(gioHang.getSoluong()));
+        Double gia = gioHang.getGiasp()*gioHang.getSoluong();
+        holder.tvGiohanggiatien.setText(String.valueOf(gia));
+        //xu li + -
+        holder.setIimageClickListen(new IimageClickListen() {
+            @Override
+            public void onImageClick(View view, int pos, int giatri) {
+                Log.e("click:","da nhan");
+                if (giatri==1){
+                    if (giohangList.get(pos).getSoluong()>1){
+                        int soluongmoi = giohangList.get(pos).getSoluong()-1;
+                        giohangList.get(pos).getSoluong(soluongmoi);
+                        Log.e("click:",String.valueOf(soluongmoi));
+                    }
+                }else if (giatri==2){
+                    if (giohangList.get(pos).getSoluong()<11){
+                        int soluongmoi = giohangList.get(pos).getSoluong()+1;
+                        giohangList.get(pos).getSoluong(soluongmoi);
+                    }
+                }
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
+        if (giohangList != null) {
+            return giohangList.size();
+        }
         return 0;
     }
 
-    public class MyViewHoler extends RecyclerView.ViewHolder{
+
+    public class MyViewHoler extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView tvGiohangtensp,tvGiohanggiasp,tvGiohangsoluong,tvGiohanggiatien;
+        ImageView imgGiohangimgsp,imgGiohangtru,imgGiohangcong;
+        IimageClickListen iimageClickListen;
 
         public MyViewHoler(@NonNull View itemView) {
             super(itemView);
+            tvGiohangtensp = itemView.findViewById(R.id.item_giohang_tensp);
+            tvGiohanggiasp = itemView.findViewById(R.id.item_giohang_gia);
+            tvGiohangsoluong = itemView.findViewById(R.id.item_giohang_soluong);
+            tvGiohanggiatien = itemView.findViewById(R.id.item_giohang_giasp2);
+            imgGiohangimgsp = itemView.findViewById(R.id.item_giohang_image);
+            //xu li dau + -
+            imgGiohangtru = itemView.findViewById(R.id.item_giohang_tru);
+            imgGiohangcong = itemView.findViewById(R.id.item_giohang_cong);
+            imgGiohangtru.setOnClickListener(this);
+            imgGiohangcong.setOnClickListener(this);
+        }
+
+        public void setIimageClickListen(IimageClickListen iimageClickListen) {
+            this.iimageClickListen = iimageClickListen;
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (view == imgGiohangtru){
+                iimageClickListen.onImageClick(view,getAdapterPosition(),1);
+            }else if (view == imgGiohangcong){
+                iimageClickListen.onImageClick(view,getAdapterPosition(),2);
+            }
         }
     }
+
+
 }
--- app/src/main/java/org/sonnnph12414/appduantotnghiep/fragment/GioHangFragment.java
+++ app/src/main/java/org/sonnnph12414/appduantotnghiep/fragment/GioHangFragment.java
@@ -1,39 +1,35 @@
 package org.sonnnph12414.appduantotnghiep.fragment;
 
-import static org.sonnnph12414.appduantotnghiep.api.APIClient.retrofit;
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import org.sonnnph12414.appduantotnghiep.R;
+import org.sonnnph12414.appduantotnghiep.adapter.GioHangAdapter;
 import org.sonnnph12414.appduantotnghiep.api.APIClient;
-import org.sonnnph12414.appduantotnghiep.api.APIClientlpm;
-import org.sonnnph12414.appduantotnghiep.model.Foodbuy;
+import org.sonnnph12414.appduantotnghiep.model.GioHang;
 
 import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.Currency;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
+import java.util.List;
 
 public class GioHangFragment extends Fragment {
     TextView giohangtrong, tvSum;
     Toolbar toolbar;
     RecyclerView recyclerView;
     Button btnmuahang;
+    List<GioHang> gioHangList;
 
     public GioHangFragment(){}
 
@@ -42,13 +38,11 @@ public class GioHangFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_gio_hang,container,false);
 
-        giohangtrong = view.findViewById(R.id.txtgiohangtrong);
-        // toolbar = view.findViewById(R.id.toolbar);
+        giohangtrong = view.findViewById(R.id.txtgiohangtrong);;
         recyclerView = view.findViewById(R.id.recyclerviewgiohang);
         tvSum = view.findViewById(R.id.txttongtien);
         btnmuahang = view.findViewById(R.id.btnmuahang);
 
-        //return super.onCreateView(inflater, container, savedInstanceState);
 
         initData();
         btnmuahang.setOnClickListener(v ->{
@@ -63,6 +57,16 @@ public class GioHangFragment extends Fragment {
                     .addToBackStack(null)
                     .commit();
         });
+        //Xong - Adapter cho mang giohang
+        gioHangList = new ArrayList<>();
+        gioHangList = APIClient.manggiohang;
+        for (int i=0;i<gioHangList.size();i++){
+            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(), RecyclerView.VERTICAL, false);
+            GioHangAdapter gioHangAdapter = new GioHangAdapter(gioHangList,getActivity());
+            recyclerView.setLayoutManager(linearLayoutManager);
+            recyclerView.setAdapter(gioHangAdapter);
+        }
+
         return view;
     }
 
@@ -82,22 +86,22 @@ public class GioHangFragment extends Fragment {
 
         tvSum.setText(format.format(sum));
         //POST retrofit
-        /*Foodbuy foodbuy = new Foodbuy();
-        APIClient.getClient("api/cart/:id");
-        APIClientlpm apiClientlpm = retrofit.create(APIClientlpm.class);
-        Call<Foodbuy> call=apiClientlpm.updategio(foodbuy.getFoodName(),String.valueOf(price),String.valueOf(sum));
-        call.enqueue(new Callback<Foodbuy>() {
-            @Override
-            public void onResponse(Call<Foodbuy> call, Response<Foodbuy> response) {
-                Foodbuy foodbuy1 =response.body();
-                // Toast.makeText(getContext(),"foodbuy1.getMessage()",Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onFailure(Call<Foodbuy> call, Throwable t) {
-                Toast.makeText(getContext(),t.getMessage(),Toast.LENGTH_SHORT).show();
-            }
-        });*/
+//        Foodbuy foodbuy = new Foodbuy();
+//        APIClient.getClient("api/cart/:id");
+//        APIClientlpm apiClientlpm = retrofit.create(APIClientlpm.class);
+//        Call<Foodbuy> call=apiClientlpm.updategio(foodbuy.getFoodName(),String.valueOf(price),String.valueOf(sum));
+//        call.enqueue(new Callback<Foodbuy>() {
+//            @Override
+//            public void onResponse(Call<Foodbuy> call, Response<Foodbuy> response) {
+//                Foodbuy foodbuy1 =response.body();
+//                 Toast.makeText(getContext(),"foodbuy1.getMessage()",Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onFailure(Call<Foodbuy> call, Throwable t) {
+//                Toast.makeText(getContext(),t.getMessage(),Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
     }
 
